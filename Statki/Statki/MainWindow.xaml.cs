6497aef1b2d7e82a2af39d97f8608c1e45682907
KacperM27 <kacpermusiaka@gmail.com>
2025-03-18 10:38:12 +0100

    Project start

---
+++ Statki/Statki/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Statki
+{
+    public partial class MainWindow : Window
+    {
+        public int level;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EasyButton_Click(object sender, RoutedEventArgs e)
+        {
+            level = 1; 
+            OpenSinglePlayerShipsWindow();
+        }
+
+        private void MediumButton_Click(object sender, RoutedEventArgs e)
+        {
+            level = 2; 
+            OpenSinglePlayerShipsWindow();
+        }
+
+        private void MultiplayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            level = 0;  
+            OpenSinglePlayerShipsWindow();
+        }
+
+       
+        private void OpenSinglePlayerShipsWindow()
+        {
+            SinglePlayerShips singlePlayerShips = new SinglePlayerShips(level);
+            singlePlayerShips.Show();
+            this.Close();  
+        }
+    }
+}
---
+++ Statki/Statki/SinglePlayerShips.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Statki
+{
+    public partial class SinglePlayerShips : Window
+    {
+        private int level;
+
+        public SinglePlayerShips(int level)
+        {
+            InitializeComponent();
+            this.level = level;
+            GenerateAndDisplayBoard();
+        }
+
+        public void GenerateAndDisplayBoard()
+        {
+            int[][] board = new int[11][];
+
+            for (int i = 0; i < 11; i++)
+            {
+                board[i] = new int[11];
+                for (int j = 0; j < 11; j++)
+                {
+                    if (i == 0 || j == 0) {
+                        board[i][j] = -1;
+                    }
+                    else {
+                        board[i][j] = 0; 
+                    }
+                }
+            }
+
+            DisplayBoard(board);
+        }
+
+        public void DisplayBoard(int[][] board)
+        {
+            BoardGrid.Children.Clear();
+
+            for (int i = 0; i < 11; i++)
+            {
+                BoardGrid.RowDefinitions.Add(new RowDefinition());
+                BoardGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+
+
+                    if (board[i][j] == -1 && i == 0)
+                    {
+                        Button btn = new Button
+                        {
+                            Content = Convert.ToChar(j + 64).ToString(),
+                            Background = Brushes.White,
+                            BorderBrush = Brushes.White
+
+
+                        };
+                        btn.IsEnabled = false;
+                        Grid.SetRow(btn, i);
+                        Grid.SetColumn(btn, j);
+                        BoardGrid.Children.Add(btn);
+                    }
+                    if (board[i][j] == -1 && j == 0)
+                    {
+                        Button btn = new Button
+                        {
+                            Content = Convert.ToString(i),
+                            Background = Brushes.White,
+                            BorderBrush = Brushes.White
+
+
+                        };
+                        btn.IsEnabled = false;
+                        Grid.SetRow(btn, i);
+                        Grid.SetColumn(btn, j);
+                        BoardGrid.Children.Add(btn);
+                    }
+                    if(board[i][j] == 0)
+                    {
+                        Button btn = new Button
+                        {
+                            Background = Brushes.LightBlue,
+                            BorderBrush = Brushes.Black,
+
+                        };
+
+                        Grid.SetRow(btn, i);
+                        Grid.SetColumn(btn, j);
+                        BoardGrid.Children.Add(btn);
+                    }
+                    if (i == 0 && j == 0)
+                    {
+                        Button btn = new Button
+                        {
+                            Background = Brushes.White,
+                            BorderBrush = Brushes.White
+
+                        };
+                        btn.IsEnabled = false;
+                        Grid.SetRow(btn, i);
+                        Grid.SetColumn(btn, j);
+                        BoardGrid.Children.Add(btn);
+                    }
+                }
+            }
+        }
+    }
+}
